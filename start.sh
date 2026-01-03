8547fbca1f7b2764c242f4328e55799f5de6f847
palesos5 <lol111h@yandex.ru>
2026-01-03 22:46:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-echo apt update && apt install proot-distro && bash install.sh && proot-distro install alt
+echo apt update && apt install proot-distro && bash install.sh && proot-distro install alt && proot-distro login alt
