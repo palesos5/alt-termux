2f9a41eac29be516ea8c7fd05d48991f69a66dbd
palesos5 <lol111h@yandex.ru>
2026-01-05 00:14:59 +0300

    Update postmarketOS.sh

--- distros/postmarketOS.sh
+++ distros/postmarketOS.sh
@@ -1,27 +1,17 @@
-DISTRO_NAME="postmarketOS"
-DISTRO_COMMENT="Linux для мобильных устройств (ветка edge, qemu-generic)"
+DISTRO_NAME="PostmarketOS"
+DISTRO_COMMENT="V25.12"
 
-# Ссылки на актуальные образы (edge)
-TARBALL_URL['aarch64']="https://images.postmarketos.org/rootfs/edge/qemu-aarch64/postmarketos-qemu-aarch64.tar.gz"
-# Хэш можно проверить здесь: https://images.postmarketos.org/rootfs/edge/qemu-aarch64/postmarketos-qemu-aarch64.tar.gz.sha256
-TARBALL_SHA256['aarch64']="1670987178652d3a334d55734538806289945089774653653139886367339736"
-
-TARBALL_URL['arm']="https://images.postmarketos.org/rootfs/edge/qemu-vexpress/postmarketos-qemu-vexpress.tar.gz"
-TARBALL_SHA256['arm']="4567298374928374928374928374928374928374928374928374928374928374" # Замените на актуальный из .sha256
-
-TARBALL_URL['i686']="https://images.postmarketos.org/rootfs/edge/qemu-i686/postmarketos-qemu-i686.tar.gz"
-TARBALL_SHA256['i686']="1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef" # Замените на актуальный из .sha256
-
-TARBALL_URL['x86_64']="https://images.postmarketos.org/rootfs/edge/qemu-amd64/postmarketos-qemu-amd64.tar.gz"
-TARBALL_SHA256['x86_64']="9876543210fedcba9876543210fedcba9876543210fedcba9876543210fedcba" # Замените на актуальный из .sha256
+TARBALL_URL['aarch64']="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.2-aarch64.tar.gz"
+TARBALL_SHA256['aarch64']="0a6c278a906bb7dc1c14f3dae426b4494280cdc421697977459bd37f91287d09"
+TARBALL_URL['arm']="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/armv7/alpine-minirootfs-3.23.2-armv7.tar.gz"
+TARBALL_SHA256['arm']="79603b670dc749b494aabbcfbb34ce74ae6e4e289e78e3738d7ddeb57fe9b6f0"
+TARBALL_URL['x86_64']="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86_64/alpine-minirootfs-3.23.2-x86_64.tar.gz"
+TARBALL_SHA256['x86_64']="b50bf42e519420ca2be48dd0efa22aa087c708d0602b67d413406533bef9dab5"
 
 distro_setup() {
-    # Обновляем репозитории и систему после первого запуска
-    run_proot_cmd apk update
-    run_proot_cmd apk upgrade
-
-    # Установка базовых инструментов, если они отсутствуютrun_proot_cmd apk add bash nano coreutils
-
-    # Исправление DNS (если необходимо в некоторых средах proot)
-    echo "nameserver 1.1.1.1" > "${ROOTFS_DIR}/etc/resolv.conf"
+        run_proot_cmd sh -c 'echo "http://mirror.postmarketos.org/postmarketos/v25.12/" >> /etc/apk/repositories'
+        run_proot_cmd sh -c 'apk add -u --allow-untrusted postmarketos-keys'
+        run_proot_cmd sh -c 'apk update && apk upgrade -a'
+        run_proot_cmd sh -c 'apk add -q curl'
+        run_proot_cmd sh -c 'curl https://porkof48.ru/linux/postmarket_os.os-release > /etc/os-release'
 }
